92a32cdd5dff9eda016b785dcb3f4965ed650f25
ItsJustPanPan <cris-kohn@live.dk>
2023-06-15 23:40:47 +0200

    Add project files.

---
+++ Eksempel 1/Items/Bows/Long Bow/Long Bow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksempel_1.Items.Bows.Long_Bow
+{
+    internal class Long_Bow : Bow
+    {
+    }
+}
